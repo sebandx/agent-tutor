84e03a97c9f918f64ddc067610f21297d09094cb
inkay_liu <enkai.liu.1@gmail.com>
2025-06-14 20:07:25 -0400

    moved question_tool into its own file

--- orchestrator/agent.py
+++ orchestrator/agent.py
@@ -4,33 +4,11 @@ from question_agent.agent import question_agent, questionInput
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
 from google.genai.types import Content, Part
+from tools.question import question_tool
 
 import json
 from typing import Literal 
 
-
-async def question_tool(question_type: str, question_difficulty: Literal["easy","medium","hard"]) -> dict:
-    """
-    Use this tool when a student asks for a CS practice problem. 
-    It calls a specialist agent to generate a structured question and returns a python dictionary. 
-    """
-    question_agent_runner = Runner(agent=question_agent, session_service=InMemorySessionService(), app_name = "agent_tutor_app")
-
-    input = questionInput(type=question_type, difficulty=question_difficulty).model_dump_json()
-
-    user_content = Content(role='user', parts=[Part(text=input)])
-
-    await question_agent_runner.session_service.create_session(
-        app_name=question_agent_runner.app_name,
-        user_id="sub_call_user",
-        session_id="sub_call_session"
-    )
-
-    async for event in question_agent_runner.run_async(user_id="sub_call_user", session_id="sub_call_session", new_message=user_content):
-        if event.is_final_response() and event.content and event.content.parts:
-            return json.loads(event.content.parts[0].text)
-        
-    return {"error": "The question agent did not return a valid response."}
 
 root_agent = LlmAgent(
     model="gemini-2.0-flash",
---
+++ tools/question.py
@@ -0,0 +1,46 @@
+# *** STEPS ON WHAT TO DO NEXT ***
+# MOVE ALL FUNCTIONS INTO THEIR OWN "TOOLS FOLDER"
+# THEN MAKE A TOOL TO RUN RACKET 
+
+# STEP #1, MOVE ALL FUNCTIONS INTO TOOL FOLDER
+
+import json
+from typing import Literal
+
+# Imports needed for the tool to function
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+from google.genai.types import Content, Part
+
+# Import the agent and schemas the tool needs to call
+from question_agent.agent import question_agent, questionInput
+
+
+async def question_tool(question_type: str, question_difficulty: Literal["easy","medium","hard"]) -> dict:
+    """
+    Use this tool when a student asks for a CS practice problem. 
+    It calls a specialist agent to generate a structured question and returns a python dictionary. 
+    """
+    question_agent_runner = Runner(agent=question_agent, session_service=InMemorySessionService(), app_name = "agent_tutor_app")
+
+    input = questionInput(type=question_type, difficulty=question_difficulty).model_dump_json()
+
+    user_content = Content(role='user', parts=[Part(text=input)])
+
+    # We can use a fixed session for these internal sub-calls
+    session_id = "question_generator_session"
+    user_id = "internal_tool_user"
+
+    await question_agent_runner.session_service.create_session(
+        app_name=question_agent_runner.app_name,
+        user_id=user_id,
+        session_id=session_id
+    )
+
+    async for event in question_agent_runner.run_async(user_id=user_id, session_id=session_id, new_message=user_content):
+        if event.is_final_response() and event.content and event.content.parts:
+            return json.loads(event.content.parts[0].text)
+        
+    return {"error": "The question agent did not return a valid response."}
+
+
